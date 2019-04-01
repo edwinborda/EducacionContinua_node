34ced6f22e5db93fe684b4763f9b4b60e5c0e0fc
Edwin Borda <edwin.borda@outlook.com>
2019-04-01 12:33:06 -0500

    se crea vista de crear cursos

--- src/app.js
+++ src/app.js
@@ -8,12 +8,13 @@ const node_modulesPath = path.join(__dirname, '../node_modules');
 const publicPath = path.join(__dirname, '../public')
 const partialsPath = path.join(__dirname, '../partials')
 
-app.use(express.static(publicPath));
 /*bootstrap modules */
 app.use('/css', express.static(node_modulesPath + '/bootstrap/dist/css'));
 app.use('/js', express.static(node_modulesPath + '/jquery/dist'));
 app.use('/js', express.static(node_modulesPath + '/popper.js/dist'));
 app.use('/js', express.static(node_modulesPath + '/bootstrap/dist/js'));
+/*Custom static files */
+app.use(express.static(publicPath));
 /*handlebars */
 app.set('view engine', 'hbs')
 hbs.registerPartials(partialsPath);
@@ -21,6 +22,11 @@ hbs.registerPartials(partialsPath);
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/create', (req, res) => {
+    res.render('createCourse');
+});
+
 
 app.listen(3000, ()=>{
     console.log('listen on port 3000');
